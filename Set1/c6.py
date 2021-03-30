932d8e31e5f40a74e31a6ce2f941b3e1b897038c
joaopcg10 <36642960+joaopcg10@users.noreply.github.com>
2021-03-30 22:47:53 +0100

    Challenge 6 completed
    Actual key size still isn't determined but its easy to choose one from a list of 4

--- Set1/c6.py
+++ Set1/c6.py
@@ -18,35 +18,52 @@ def hamming(str1,str2):
 
     return hamming
 
+def min_hamming(hamming_list):
+    key_size = 0
+    min_ham = 1000
+
+    for i in len(range(hamming_list)):
+        if hamming_list[i][0] < min_ham:
+            key_size = hamming_list[i][1]
+
+    return key_size
+
+
 def main():
     file = open("c6.txt", "r")
     encoded = base64.b64decode(file.read())
-    print(encoded)
     min_list = []
+
     for key_size in range(2,40):
-        first = encoded[0:key_size]
-        second = encoded[key_size:key_size*2]
-        min_list.append(int(hamming(first,second) / key_size))
+        for i in range(0,len(encoded)-key_size*2,key_size):
+            tmp_list = []
+            first = encoded[i:i+key_size]
+            second = encoded[i+key_size:i+(key_size*2)]
+            tmp_list.append(hamming(first,second)/key_size)
+        normalized_hamming = (sum(tmp_list) / len(tmp_list))
+        min_list.append((normalized_hamming,key_size))
+    
+    sorted_list = sorted(min_list, key=lambda tup: tup[0])
+    possible_key_sizes = [key[1] for key in sorted_list[:4]]
 
-    print(min_list)
-    key_size = 29
-    print("Key Size: " + str(key_size))
+    keys = []
+    for key_size in possible_key_sizes:
+        encoded_chunks = []
+        for i in range(0,len(encoded),key_size):
+            encoded_chunks.append(encoded[i:i+key_size])
 
-    encoded_chunks = []
-    for i in range(0,len(encoded),key_size):
-        encoded_chunks.append(encoded[i:i+key_size])
-
-    encoded_blocks = [b''] * key_size
-    for chunk in encoded_chunks:
-            for byte in range(len(chunk)):
-                encoded_blocks[byte] += bytes([chunk[byte]])
+        encoded_blocks = [b''] * key_size
+        for chunk in encoded_chunks:
+                for byte in range(len(chunk)):
+                    encoded_blocks[byte] += bytes([chunk[byte]])
 
     
-    key = ""
-    for block in encoded_blocks:
-        key += detect_single_byte_xor(block.hex())[1]
+        key = ""
+        for block in encoded_blocks:
+            key += detect_single_byte_xor(block.hex())[1]
 
-    print("Key: " + key)
-    
+        keys.append(key)
+
+    print("Possible keys are: " + str(keys))
 if __name__ == "__main__":
     main()
